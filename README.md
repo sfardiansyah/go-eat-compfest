adde30fe1748157988fc546c7056a7baed329125
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2019-06-23 00:00:39 +0700

    Generate Menu

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Go-Eat
+
 ## By: Syahrul Findi Ardiansyah
+
+Assumptions:
+
+- Every store will have between 2-10 items with price in range Rp 10.000-50.000
--- lib/go_eat/store.rb
+++ lib/go_eat/store.rb
@@ -1,11 +1,25 @@
 require_relative 'instance'
 
 class Store < Instance
+  attr_reader :menu
+  
   def initialize(coordinate)
     super(coordinate)
+    @menu = generate_menu(rand(2..10))
   end
 
   def to_s
     "S"
+  end
+
+  def print_menu
+    @menu.each_with_index do |price, i|
+      puts "- Item #{i+1}: Rp #{price}"
+    end
+  end
+
+  def generate_menu(n)
+    menu = Array.new(n) {rand(100..500) * 100}
+    return menu
   end
 end
